3a6226797e61f371b88764f5fa1f012bdfa8b9dc
Kartik Shanbhag <kshanbhag231@gmail.com>
2025-06-27 13:31:26 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,33 +2,38 @@ import streamlit as st
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import re
 
 @st.cache_data(show_spinner=False)
 def get_all_players():
-    res = requests.get("https://fbref.com/en/players/")
+    url = "https://fbref.com/en/players/"
+    res = requests.get(url)
     soup = BeautifulSoup(res.text, "lxml")
     links = soup.select("div#content a[href^='/en/players/']")
 
     player_dict = {}
     for a in links:
-        href = a.get("href", "")
         name = a.text.strip()
-        if href.count("/") == 4:  # /en/players/{id}/{name}
+        href = a.get("href", "")
+        # Valid player profile: /en/players/{id}/{name}
+        if re.match(r"^/en/players/[a-zA-Z0-9]{8}/.+", href) and len(name) > 3:
             full_url = "https://fbref.com" + href
             player_dict[name] = full_url
     return player_dict
 
 @st.cache_data(show_spinner=False)
 def get_all_teams():
-    res = requests.get("https://fbref.com/en/squads/")
+    url = "https://fbref.com/en/squads/"
+    res = requests.get(url)
     soup = BeautifulSoup(res.text, "lxml")
     links = soup.select("div#content a[href^='/en/squads/']")
 
     team_dict = {}
     for a in links:
-        href = a.get("href", "")
         name = a.text.strip()
-        if href.count("/") == 4:  # /en/squads/{id}/{name}
+        href = a.get("href", "")
+        # Valid team profile: /en/squads/{id}/{team-name}
+        if re.match(r"^/en/squads/[a-zA-Z0-9]{8}/[a-zA-Z0-9\-]+$", href):
             full_url = "https://fbref.com" + href
             team_dict[name] = full_url
     return team_dict
@@ -36,9 +41,9 @@ def get_all_teams():
 @st.cache_data(show_spinner=False)
 def get_player_stats(url):
     tables = pd.read_html(url)
-    return tables[0]  # update if needed
+    return tables[0] if tables else pd.DataFrame()
 
 @st.cache_data(show_spinner=False)
 def get_team_stats(url):
     tables = pd.read_html(url)
-    return tables[0]  # update if needed
+    return tables[0] if tables else pd.DataFrame()
